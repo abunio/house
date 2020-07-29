dd6ca1f09de1b7906dad15908bfb21472deb61bf
abunio <abunio123456>
2020-07-29 14:48:33 +0800

    代码优化

---
+++ h-common/src/main/java/com/house/common/utils/json/JsonUtils.java
@@ -0,0 +1,52 @@
+package com.house.common.utils.json;
+
+/**
+ * @Description
+ * @Author huangW
+ * @Date 10:03 2020/7/18
+ * @Version 1.0
+ */
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+    // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * 将对象转换成json字符串。
+     */
+    public static String toStr(Object data) {
+        try {
+            String string = MAPPER.writeValueAsString(data);
+            return string;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 将json结果集转化为对象
+     */
+    public static <T> T toPoJo(String jsonData, Class<T> beanType) {
+        try {
+            T t = MAPPER.readValue(jsonData, beanType);
+            return t;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 将json数据转换成pojo对象list
+     */
+    public  static <T> T toList(String jsonData,TypeReference<T> typeReference) {
+        try {
+            return MAPPER.readValue(jsonData, typeReference);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
